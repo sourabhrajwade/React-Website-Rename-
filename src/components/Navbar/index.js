fded205106bfc7b9f34bc91efd85b7112218e7c4
Sourabh Rajwade <rajwadesourabh@gmail.com>
2021-01-14 14:49:57 +0530

    add navbar and sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import {Nav, NavLogo, NavbarContainer} from  './NavbarEle';
+import React from "react";
+import { FaBars } from 'react-icons/fa';
+import {
+  Nav,
+  NavLogo,
+  NavbarContainer,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from "./NavbarEle";
 
 const Navabr = () => {
-    return (
-        <>
-           <Nav>
-               <NavbarContainer>
-                   <NavLogo to='/'>
-                       Yolo
-                   </NavLogo>
-               </NavbarContainer>
-           </Nav>
-        </>
-    )
-}
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">Yolo</NavLogo>
+          <MobileIcon>
+              <FaBars />
+          </MobileIcon>
+          <NavMenu>
+                <NavItem>
+                    <NavLinks to='about'>About</NavLinks>
+                </NavItem>
+                <NavItem>
+                    <NavLinks to='discover'>Discover</NavLinks>
+                </NavItem>
+                <NavItem>
+                    <NavLinks to='services'>Services</NavLinks>
+                </NavItem>
+                <NavItem>
+                    <NavLinks to='signup'>Sign Up</NavLinks>
+                </NavItem>
+          </NavMenu>
+          <NavBtn>
+              <NavBtnLink to='/signin'>
+                Sign In
+              </NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
 
-export default Navabr
+export default Navabr;
